489a9b5334f080b18e53aaa4f3f5326a62ff1df8
Mohammed Mazahim <mazamim91@gmail.com>
2022-01-02 08:41:00 +1100

    updated city

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ var temp = document.querySelector("#temp")
 var humidity = document.querySelector("#humidity")
 var windy = document.querySelector("#wind")
 var uv = document.querySelector("#uv")
+var cDescription = document.querySelector("#c-description")
 
+var searchForm = document.querySelector('#search-form');
+var cityInput = document.querySelector('#cityInput');
+var citylistItem = document.querySelector('#citylistItem');
+
+var cityList=[]
+
+//1
 getGeoLocation()
+
 function getGeoLocation(){
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position)=>{
@@ -30,6 +39,7 @@ var local= JSON.parse(localStorage.getItem("weather-data"));
     {
    
       var date2 = moment.unix(local.current.dt).format("MM/DD/YYYY");
+      date2=''
     
     }
     if (local!=null && date1 === date2){
@@ -76,9 +86,12 @@ var local= JSON.parse(localStorage.getItem("weather-data"));
 
 function display(data){
   if (data!=null){
+    console.log(data)
       const {current}=data
       // h1Place.textContent=
+      console.log(data.current)
         temp.textContent=current.temp
+        cDescription.textContent=current.weather[0].description
          humidity.textContent=current.humidity
          windy.textContent=current.wind_speed
        uv.textContent=current.uvi
@@ -87,18 +100,25 @@ function display(data){
  }
 
 
- function addressToGeoCode(city){
+function addressToGeoCode(city){
 var url ='https://maps.googleapis.com/maps/api/geocode/json?address='+city+'&key=AIzaSyBi2s5puIfi0U5S0NRdR4NiprHdtQf2JFA'
 var geo;
 
- fetch(url)
+fetch(url)
 .then(function (response) {
   if (response.ok) {
-   response.json().then(function (data) {
+   response.json().then( function (data) {
   if (data !=null){
 
     geo = data.results[0].geometry.location
-     
+ 
+    loadIni(geo.lat,geo.lng)
+
+
+   // geoToAddress()
+  
+  
+
       }   
     });
   } else {
@@ -109,11 +129,12 @@ var geo;
   alert('Unable to connect to google API');
 });
 
-return geo
+
  
 }
 
- function geoToAddress(lat,lon){
+  function geoToAddress(lat,lon){
+   console.log(lat+" "+ lon)
   var url ='https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon+'&key=AIzaSyBi2s5puIfi0U5S0NRdR4NiprHdtQf2JFA'
  fetch(url)
   .then(function (response) {
@@ -134,10 +155,15 @@ return geo
 
         }
         }
-        h1Place.textContent=city.long_name
-    
-      
-       
+        console.log(city.long_name)
+        h1Place.textContent=city.long_name + ' - ' +moment().format('Dd/M/yyyy')
+        cityList.push(city.long_name)
+
+        let uniqueCity = cityList.filter((c, index) => {
+          return cityList.indexOf(c) === index;
+      });
+        createListCities(uniqueCity)
+       console.log(uniqueCity)
         }   
       });
     } else {
@@ -160,10 +186,13 @@ return geo
 
 
     fData.forEach((element)=>{
-      console.log(element)
+      // console.log(element)
       const {dt,uvi,weather,temp,humidity}=element
      newdt= moment.unix(dt).format("DD/MM/YYYY")
-      //create elements
+      
+     //remove previous elemnts
+  
+     //create elements
       createElements(newdt,temp.max,humidity,weather)
 
     })
@@ -178,116 +207,122 @@ return geo
 
   //create elements
   const parentDiv =document.querySelector('#parentDiv')
-  const outerDiv=document.createElement('div')
-  outerDiv.classList.add('card' ,'text-white' ,'bg-primary' ,'mb-3')
-  outerDiv.setAttribute("style","max-width: 10rem;")
-
-  const cardHeader=document.createElement('div')
-  cardHeader.classList.add('card-header')
-
-  const cardBody=document.createElement('div')
-  cardBody.classList.add('card-body')
-
-  const innerH5=document.createElement('h5')
-  innerH5.classList.add('card-title')
-  const iclass=document.createElement('i')
-  const ispanDes=document.createElement('span')
-  ispanDes.setAttribute('style','font-size:0.8em;')
-  
-  
- 
-
-  const innerTempP=document.createElement('p')
-  innerTempP.classList.add('card-text')
-  const innerSpanTemp=document.createElement("span")
-  innerSpanTemp.setAttribute('style','font-size:0.8em;')
-  ispanDes.setAttribute("style","color:#f9d71c; font-size:0.8rem")
-
-  const innerHumidityP=document.createElement('p')
-  innerHumidityP.classList.add('card-text')
-  innerHumidityP.setAttribute('style','font-size:0.8em;')
-
-  // required style
+  //
 
 //   <div class="card text-white bg-primary mb-3" style="max-width: 8rem;">
+const outerDiv=document.createElement('div')
+    outerDiv.classList.add('card' ,'text-white' ,'bg-primary' ,'mb-3')
+    outerDiv.setAttribute("style","max-width: 10rem;")
+    parentDiv.appendChild(outerDiv);       
 //   <div class="card-header">25/12/2021</div>
+const cardHeader=document.createElement('div')
+cardHeader.classList.add('card-header')
+outerDiv.appendChild(cardHeader);
+cardHeader.textContent=cardheader
 //   <div class="card-body">
-//     <h5 class="card-title"><i class="fa fa-sun-o" aria-hidden="true"></i></h5>
+const cardBody=document.createElement('div')
+cardBody.classList.add('card-body')
+outerDiv.appendChild(cardBody);
+//     <h5 class="card-title"></h5>
+const innerH5=document.createElement('h5')
+innerH5.classList.add('card-title')
+
 //     <p class="card-text">Temp: <span>86.84</span>&#8451;</p>
+const innerTempP=document.createElement('p')
+innerTempP.classList.add('card-text')
+cardBody.appendChild(innerTempP);
+const innerSpanTemp=document.createElement("span")
+innerSpanTemp.setAttribute('style','font-size:0.8em;')
+innerSpanTemp.innerHTML='Temp : '+temp+' &deg;C';
+
 //     <p class="card-text">Humidity: <span>43%</span></p>
+
+const innerHumidityP=document.createElement('p')
+innerHumidityP.classList.add('card-text')
+innerHumidityP.setAttribute('style','font-size:0.8em;')
+const ispanDes=document.createElement('span')
+// ispanDes.setAttribute('style','font-size:0.8em;')
+ispanDes.setAttribute("style","color:#f9d71c; font-size:0.8rem")
+innerTempP.appendChild(innerSpanTemp)   
+cardBody.appendChild(innerHumidityP);
+innerHumidityP.innerHTML='Humidity : '+humidity+ '%'
 //   </div>
 // </div>
 
-parentDiv.appendChild(outerDiv);
-outerDiv.appendChild(cardHeader);
-outerDiv.appendChild(cardBody);
-      
-cardBody.appendChild(innerH5);
-innerH5.appendChild(iclass);
-iclass.appendChild(ispanDes);
-cardBody.appendChild(innerTempP);
-  innerTempP.appendChild(innerSpanTemp)    
-cardBody.appendChild(innerHumidityP);
 
-cardHeader.textContent=cardheader
-innerSpanTemp.innerHTML='Temp : '+temp+' &deg;C';
-innerHumidityP.innerHTML='Humidity : '+humidity+ '%'
-if(weather[0].description==='clear sky'){
-  iclass.classList.add('fa','fa-certificate')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' clear sky'
+const img = document.createElement("img");
+cardBody.appendChild(img)
+  img.src = 'http://openweathermap.org/img/wn/'+weather[0].icon+'@2x.png';
+
  
-}
-
-if(weather[0].description==='few clouds'){
-  iclass.classList.add('fa','fa-cloud-download')
-  iclass.setAttribute("style","color:#231F20;")
-  ispanDes.textContent=' few clouds'
-
-}
-
-if(weather[0].description==='overcast clouds'){
-  iclass.classList.add('fa','fa-cloud-upload')
-  iclass.setAttribute("style","color:#231F20;")
-  ispanDes.textContent=' overcast clouds'
-
-}
-
-if(weather[0].description==='drizzle'){
-  iclass.classList.add('fa','fa-spinner')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' drizzle'
-}
-
-if(weather[0].description==='rain'){
-  iclass.classList.add('fa','fa-umbrella')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' rain'
-}
-
-if(weather[0].description==='shower rain'){
-  iclass.classList.add('fa','fa-shower')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' shower rain'
-}
-
-if(weather[0].description==='thunderstorm'){
-  iclass.classList.add('fa','fa fa-bolt')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' thunderstorm'
-}
-
-if(weather[0].description==='snow'){
-  iclass.classList.add('fa','fa-snowflake-o')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' snow'
-}
-
-if(weather[0].description==='mist'){
-  iclass.classList.add('fa','fa-bath')
-  iclass.setAttribute("style","color:#f9d71c;")
-  ispanDes.textContent=' mist'
-}
-
+  const description=document.createElement('p')
+  description.classList.add('card-text')
+  cardBody.appendChild(description)
+  description.setAttribute('style','font-size:0.8em;')
+  description.textContent=weather[0].description
   }
 
+  searchForm.addEventListener('submit', formSubmitHandler);
+
+  
+function formSubmitHandler(event) {
+  event.preventDefault();
+  //remove cards
+  //element.parentNode.removeChild(element);
+const element =document.querySelectorAll(".card")
+Array.prototype.forEach.call( element, function( node ) {
+  node.parentNode.removeChild( node );
+});
+
+const Listelements =document.querySelectorAll(".list-group-item")
+Array.prototype.forEach.call( Listelements, function( node ) {
+  node.parentNode.removeChild( node );
+});
+
+
+var geo;
+  var cityname = cityInput.value.trim();
+
+  if (cityname) {
+  geo= addressToGeoCode(cityname)
+
+ 
+  } else {
+    alert('Please enter a ');
+  }
+
+};
+
+// add citylist
+
+
+
+
+
+ function createListCities(cityListLocal){
+
+  cityListLocal.forEach((element)=>{
+
+    //  <li class="list-group-item d-flex justify-content-between align-items-center">
+const li = document.createElement('li')
+li.classList.add("list-group-item" ,"d-flex" ,"justify-content-between" ,"align-items-center")
+citylistItem.appendChild(li)
+li.textContent=element
+//    A list item
+//    <span class="badge bg-primary rounded-pill">14</span>
+const listSpan=document.createElement('span')
+listSpan.classList.add('badge' ,'bg-primary' ,'rounded-pill')
+li.appendChild(listSpan)
+listSpan.innerHTML=' &deg;C';
+//    </li>
+    
+    })
+
+
+
+
+
+
+ 
+
+}
