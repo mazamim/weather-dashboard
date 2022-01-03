406f7c88b82a2a18c0e55e9278e7cd4ccaa7bff4
Mohammed Mazahim <mazamim91@gmail.com>
2022-01-03 20:44:01 +1100

    updated features all

--- app.js
+++ app.js
@@ -12,14 +12,34 @@ var cityInput = document.querySelector('#cityInput');
 var citylistItem = document.querySelector('#citylistItem');
 
 var cityList=[]
+var currentGeo;
+var currentCity;
 
-//1
+$(window).on('load', function() {
+ 
+//get geo locaTION BY DEFAUlt
 getGeoLocation()
 
-function getGeoLocation(){
+
+
+
+for (i = 0; i < window.localStorage.length; i++) {
+    key = window.localStorage.key(i);
+    if (key.slice(-12) === "weather-data") {
+      cityList.push(key.slice(0,key.indexOf('-')));
+    }
+}
+
+console.log(cityList)
+
+
+
+async function getGeoLocation(){
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position)=>{
-        loadIni(position.coords.latitude,position.coords.longitude)
+        // loadIni(position.coords.latitude,position.coords.longitude)
+        currentGeo=position
+        geoToAddress(position.coords.latitude,position.coords.longitude)
         
     });
   } else { 
@@ -27,9 +47,84 @@ function getGeoLocation(){
   }
 }
 
-function loadIni(lat,lon){
-   geoToAddress(lat, lon)
-var local= JSON.parse(localStorage.getItem("weather-data"));
+
+// change it to name
+
+function geoToAddress(lat,lon){
+
+  var url ='https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon+'&key=AIzaSyBi2s5puIfi0U5S0NRdR4NiprHdtQf2JFA'
+ const fetchme= fetch(url)
+  .then(function (response) {
+    if (response.ok) {
+     response.json().then(function (data) {
+    if (data !=null){
+
+      for (var i=0; i<data.results[0].address_components.length; i++) { 
+        for (var b=0;b<data.results[0].address_components[i].types.length;b++){
+
+          if (data.results[0].address_components[i].types[b] == "locality") { 
+            //this is the object you are looking for 
+            city= data.results[0].address_components[i]; 
+            isLoadedCity=true
+            break; 
+        } 
+
+
+        }
+        }
+       
+        h1Place.textContent=city.long_name + ' - ' +moment().format('MM/DD/YYYY')
+        currentCity=city.long_name
+        cityList.push(currentCity.toLowerCase())
+
+        let uniqueCity = cityList.filter((c, index) => {
+          return cityList.indexOf(c) === index;
+      });
+        createListCities(uniqueCity)
+     
+        }   
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  })
+  .catch(function (error) {
+    alert('Unable to connect to google API');
+  });
+ 
+
+  }
+
+});
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//check you have address
+
+function getCurrentCity () {
+  
+
+  if (currentCity != null) {
+    console.log('i have city')
+
+    loadIni(currentGeo.coords.latitude,currentGeo.coords.longitude)
+
+
+  } else {
+    console.log('i need to wait')
+    setTimeout(getCurrentCity, 300); // try again in 300 milliseconds
+  }
+}
+
+getCurrentCity();
+
+
+
+  function loadIni(lat,lon){
+  // geoToAddress(lat, lon)
+
+var local= JSON.parse(localStorage.getItem(currentCity.toLowerCase()+"-weather-data"));
     // check from local storage
     //compare today's date and local storage saved date
     var date1 = moment.unix(moment().unix()).format("MM/DD/YYYY");
@@ -39,14 +134,14 @@ var local= JSON.parse(localStorage.getItem("weather-data"));
     {
    
       var date2 = moment.unix(local.current.dt).format("MM/DD/YYYY");
-      date2=''
+  
     
     }
     if (local!=null && date1 === date2){
   
       console.log("display data from Local Storage")
-          display(local)
-          displayForecast(local.daily)
+           display(local)
+           displayForecast(local.daily)
     }
   
     else{
@@ -58,10 +153,13 @@ var local= JSON.parse(localStorage.getItem("weather-data"));
       if (response.ok) {
        response.json().then(function (data) {
      if (data !=null){
+
+      console.log(data)
          
-              display(data)
-              displayForecast(data.daily)
-              localStorage.setItem("weather-data", JSON.stringify(data));
+               display(data)
+               displayForecast(data.daily)
+              localStorage.setItem(currentCity.toLowerCase()+"-weather-data", JSON.stringify(data));
+                 
           }
         });
   
@@ -86,10 +184,10 @@ var local= JSON.parse(localStorage.getItem("weather-data"));
 
 function display(data){
   if (data!=null){
-    console.log(data)
+
       const {current}=data
       // h1Place.textContent=
-      console.log(data.current)
+
         temp.textContent=current.temp
         cDescription.textContent=current.weather[0].description
          humidity.textContent=current.humidity
@@ -133,50 +231,7 @@ fetch(url)
  
 }
 
-  function geoToAddress(lat,lon){
-   console.log(lat+" "+ lon)
-  var url ='https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lon+'&key=AIzaSyBi2s5puIfi0U5S0NRdR4NiprHdtQf2JFA'
- fetch(url)
-  .then(function (response) {
-    if (response.ok) {
-     response.json().then(function (data) {
-    if (data !=null){
 
-      for (var i=0; i<data.results[0].address_components.length; i++) { 
-        for (var b=0;b<data.results[0].address_components[i].types.length;b++){
-
-          if (data.results[0].address_components[i].types[b] == "locality") { 
-            //this is the object you are looking for 
-            city= data.results[0].address_components[i]; 
-            isLoadedCity=true
-            break; 
-        } 
-
-
-        }
-        }
-        console.log(city.long_name)
-        h1Place.textContent=city.long_name + ' - ' +moment().format('Dd/M/yyyy')
-        cityList.push(city.long_name)
-
-        let uniqueCity = cityList.filter((c, index) => {
-          return cityList.indexOf(c) === index;
-      });
-        createListCities(uniqueCity)
-       console.log(uniqueCity)
-        }   
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  })
-  .catch(function (error) {
-    alert('Unable to connect to google API');
-  });
-  
-
-   
-  }
 
   //forecast handle
 
@@ -186,7 +241,7 @@ fetch(url)
 
 
     fData.forEach((element)=>{
-      // console.log(element)
+    
       const {dt,uvi,weather,temp,humidity}=element
      newdt= moment.unix(dt).format("DD/MM/YYYY")
       
@@ -262,6 +317,9 @@ cardBody.appendChild(img)
   description.textContent=weather[0].description
   }
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
   searchForm.addEventListener('submit', formSubmitHandler);
 
   
@@ -269,7 +327,41 @@ function formSubmitHandler(event) {
   event.preventDefault();
   //remove cards
   //element.parentNode.removeChild(element);
-const element =document.querySelectorAll(".card")
+
+  removeElements()
+
+
+  var cityname = cityInput.value.trim();
+
+  if (cityname) {
+    currentCity=cityname
+  addressToGeoCode(cityname)
+
+
+
+  h1Place.textContent=currentCity + ' - ' +moment().format('MM/DD/YYYY')
+
+  cityList.push(currentCity)
+
+  let uniqueCity = cityList.filter((c, index) => {
+    return cityList.indexOf(c) === index;
+});
+  createListCities(uniqueCity)
+
+
+ 
+  } else {
+    alert('Please enter a city Name');
+  }
+
+  cityInput.value=''
+
+};
+
+// add citylist
+
+function removeElements(){
+  const element =document.querySelectorAll(".card")
 Array.prototype.forEach.call( element, function( node ) {
   node.parentNode.removeChild( node );
 });
@@ -278,24 +370,7 @@ const Listelements =document.querySelectorAll(".list-group-item")
 Array.prototype.forEach.call( Listelements, function( node ) {
   node.parentNode.removeChild( node );
 });
-
-
-var geo;
-  var cityname = cityInput.value.trim();
-
-  if (cityname) {
-  geo= addressToGeoCode(cityname)
-
- 
-  } else {
-    alert('Please enter a ');
-  }
-
-};
-
-// add citylist
-
-
+}
 
 
 
@@ -307,22 +382,37 @@ var geo;
 const li = document.createElement('li')
 li.classList.add("list-group-item" ,"d-flex" ,"justify-content-between" ,"align-items-center")
 citylistItem.appendChild(li)
-li.textContent=element
-//    A list item
+
+
+const a =document.createElement('a')
+li.appendChild(a)
+a.textContent=element
+//  <a>  A list item</a>
 //    <span class="badge bg-primary rounded-pill">14</span>
 const listSpan=document.createElement('span')
 listSpan.classList.add('badge' ,'bg-primary' ,'rounded-pill')
 li.appendChild(listSpan)
 listSpan.innerHTML=' &deg;C';
 //    </li>
-    
+  
+    li.addEventListener('click',handleListClick)
+
     })
 
 
+}
 
+function handleListClick(event){
+removeElements()
 
-
-
- 
-
+  var btnClicked = $(event.target);
+  const tempcity=btnClicked.parent('li').prevObject[0].textContent
+  city = tempcity.substr(0, tempcity.length - 3);
+  currentCity=city
+  addressToGeoCode(city)
+  h1Place.textContent=currentCity + ' - ' +moment().format('MM/DD/YYYY')
+  let uniqueCity = cityList.filter((c, index) => {
+    return cityList.indexOf(c) === index;
+});
+  createListCities(uniqueCity)
 }
